6f1d580138d46e27463b1e20e1a6efdcf283ccfd
dannypadilla <dpadilla010@gmail.com>
2019-11-27 17:00:40 -0800

    remove unused import statements

--- app/src/main/java/com/android/labellens/MainActivity.kt
+++ app/src/main/java/com/android/labellens/MainActivity.kt
@@ -1,25 +1,10 @@
 package com.android.labellens
 
-import android.Manifest
-import android.content.pm.PackageManager
-import android.graphics.Matrix
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.util.Size
-import android.view.Surface
-import android.view.TextureView
-import android.view.ViewGroup
-import android.widget.ImageButton
-import android.widget.Toast
-import androidx.camera.core.*
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import com.android.labellens.databinding.ActivityMainBinding
-import java.io.File
-import java.util.concurrent.Executors
 
 
 class MainActivity : AppCompatActivity() {
@@ -30,7 +15,7 @@ class MainActivity : AppCompatActivity() {
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
         val navController = this.findNavController(R.id.navHostFragment)
-        
+
 
     }
 
