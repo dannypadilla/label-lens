8b9c6d32583d8e9d37b4ba6aa758f2b785a5acf0
David Krystall <david.krystall@ontometrics.com>
2019-12-12 15:52:45 -0800

    added AWS support to build files, added the config for aws, made a helper class to upload images to public simple storage service bucket

--- app/src/main/java/com/android/labellens/MainActivity.kt
+++ app/src/main/java/com/android/labellens/MainActivity.kt
@@ -2,13 +2,22 @@ package com.android.labellens
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import com.android.labellens.databinding.ActivityMainBinding
+import com.amazonaws.mobile.client.AWSStartupResult
+import com.amazonaws.mobile.client.AWSStartupHandler
+import com.amazonaws.mobile.client.AWSMobileClient
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferListener
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferState
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility
+import com.amazonaws.services.s3.AmazonS3Client
+import java.io.File
 
 
 class MainActivity : AppCompatActivity() {
-
+    val LOG_TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
@@ -16,7 +25,17 @@ class MainActivity : AppCompatActivity() {
 
         val navController = this.findNavController(R.id.navHostFragment)
 
+        AWSMobileClient.getInstance().initialize(
+            this
+        ) {
+            Log.d(
+                "MainActivity",
+                "AWSMobileClient is instantiated and you are connected to AWS!"
+            )
+        }
+            .execute()
 
     }
+
 
 }
