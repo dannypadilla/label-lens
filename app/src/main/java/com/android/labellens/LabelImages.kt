d39ce233b85de9e812da668ebbf136dbad5f4ef4
dannypadilla <dpadilla010@gmail.com>
2019-12-13 16:32:18 -0800

    maths for calculating ROI

--- app/src/main/java/com/android/labellens/LabelImages.kt
+++ app/src/main/java/com/android/labellens/LabelImages.kt
@@ -19,7 +19,12 @@ import android.text.method.Touch
 import android.view.MotionEvent
 
 class LabelImages : Fragment() {
+
     val LOG_TAG = "LabelImages"
+    val image_x_res = 640
+    val image_y_res = 640
+    val scaling_factor = 80// box size
+
     private lateinit var imagePreviewView: ImageView
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -39,6 +44,85 @@ class LabelImages : Fragment() {
 
         this.imagePreviewView.setImageURI(imagePath.toUri())
         Log.d(LOG_TAG, "Presenting image: ${imagePath.toUri()}")
+
+        var x_center = -1
+        var y_center = -1
+        var x_up_left = 0
+        var y_up_left = 0
+        var x_low_right = 0
+        var y_low_right = 0
+
+
+        this.imagePreviewView.setOnTouchListener { view, motionEvent ->
+
+            // Reset values
+            x_up_left = 0
+            y_up_left = 0
+            x_low_right = 0
+            y_low_right = 0
+
+            var x_up_offset = false
+            var y_up_offset = false
+            var x_low_offset = false
+            var y_low_offset = false
+
+            x_center = motionEvent.getX().toInt()
+            y_center = motionEvent.getY().toInt()
+
+            /* Calculate bounding box for text file */
+
+            // Upper left x, y coordinate
+            x_up_left = x_center - this.scaling_factor
+            y_up_left = y_center - this.scaling_factor
+
+            // check if calculated coordinates are out of bound
+            if (x_up_left < 0) {
+                // need to handle low_right so it doesn't shrink
+                x_low_offset = true
+                x_up_left = 0
+            }
+            if (y_up_left < 0) {
+                y_low_offset = true
+                y_up_left = 0
+            }
+
+
+            // Lower Right x, y coordinate
+            x_low_right = x_center + this.scaling_factor
+            y_low_right = y_center + this.scaling_factor
+
+            if (x_low_right > this.image_x_res) {
+                x_up_offset = true
+                x_low_right = this.image_x_res
+            }
+            if (y_low_right > this.image_y_res) {
+                y_up_offset = true
+                y_low_right = this.image_y_res
+            }
+
+            // handle offsetting of other coordinate values if any of the above are changed
+            if (x_low_offset) {
+                x_low_right = this.scaling_factor
+            }
+            if (y_low_offset) {
+                y_low_right = this.scaling_factor
+            }
+
+            if (x_up_offset) {
+                x_up_left = this.image_x_res - this.scaling_factor
+            }
+            if (y_up_offset) {
+                x_up_left = this.image_y_res - this.scaling_factor
+            }
+
+
+            // Log coordinates
+            Log.d(LOG_TAG, "\nTouched coordinates \nX:${x_center}\tY:${y_center}\n")
+            Log.d(LOG_TAG, "\nCalculated coordinates \nX_up:${x_up_left}\tY_up:${y_up_left}\n")
+            Log.d(LOG_TAG, "\nCalculated coordinates \nX_low:${x_low_right}\tY_low:${y_low_right}\n")
+
+            return@setOnTouchListener true
+        }
 
         return binding.root
     }
